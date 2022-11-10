6dce1584dcf1c848f404647449b2f15a3dc95492
valentinolie24 <valenlie2409@gmail.com>
2022-11-10 14:45:52 +0700

    Update v1

--- app/src/main/java/com/si5a/kulinerkita/FragmentKeranjang.java
+++ app/src/main/java/com/si5a/kulinerkita/FragmentKeranjang.java
@@ -2,11 +2,15 @@ package com.si5a.kulinerkita;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,8 @@ public class FragmentKeranjang extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnToast;
 
     public FragmentKeranjang() {
         // Required empty public constructor
@@ -60,5 +66,18 @@ public class FragmentKeranjang extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_keranjang, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnToast = view.findViewById(R.id.btn_toast);
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Ini Adalah Fragment Keranjang", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
